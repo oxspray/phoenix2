a4ba9eb73e19a069b8d545eea77a89985c688c32
Conny Kuehne <conny.kuehne@gmail.com>
2016-03-27 12:40:49 +0200

    Improved tests that compare old vs. new way of retrieving occurrences.

--- ph2/soap/test/compare_getoccurrenceDetails_oldVsNew.php
+++ ph2/soap/test/compare_getoccurrenceDetails_oldVsNew.php
@@ -11,18 +11,17 @@ chdir('..');
 
 require_once('ph2deafel.php');
 
-//$occurrenceIds = array(608709, 391345,443133);
 $occurrenceIds = getOccurrenceIDs('sans');
 
 $withContext = true;
 $success = true;
 
 foreach ($occurrenceIds as $occurrenceId) {
-    $occ1 = getOccurrenceDetailsOld($occurrenceId, $withContext);
-    $occ2 = getOccurrenceDetails($occurrenceId, $withContext);
+    $occOld = getOccurrenceDetailsOld($occurrenceId, $withContext);
+    $occNew = getOccurrenceDetails($occurrenceId, $withContext);
 
     // we compare based on soap object enc_value since php '==' operator is buggy; seriously shitty php
-    if ($occs1 != $occs2 || $occ1->enc_value != $occ2->enc_value){
+    if ($occOld->enc_value != $occNew->enc_value){
         echo "Failure!\n";
         $success = false;
         break;
