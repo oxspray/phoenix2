89c1db1dc9e205da2a0db0be7ac79d88e2796cef
Linus <linus.manser@gmail.com>
2017-05-09 13:55:26 +0200

    added reassign_occurrence.tab.php to have a dedicated menu to reassign occurrences within 'annotation'

---
+++ ph2/includes/components/tabs/reassign_occurrence.tab.php
@@ -0,0 +1,22 @@
+<?php
+/* Phoenix2
+** Modal Window
+==
+This is the user interface to reassign occurrences.
+*/
+
+# MOCKUP! This is a mockup prototype.
+
+?>
+<script type="text/javascript">
+
+</script>
+<div>
+    <form id="reassign_variant_form">
+        <fieldset>
+            <label class="inline" for="f1">The selected Occurrences will be reassigned to Variant Number:</label>
+            <input type="text" class="text tiny required digits_only" name="new_Number" value="" />
+            <input type="button" id="reassign_occurrences" class="button" value="Reassign" />
+        </fieldset>
+    </form>
+</div>
